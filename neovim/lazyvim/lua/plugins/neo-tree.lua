9f711dd86265a5716f034087d951bafd94dfcd9d
posky <pjm5810@gmail.com>
2023-09-26 16:02:42 +0900

    feat: neo-tree hidden file visible

--- neovim/lazyvim/lua/plugins/neo-tree.lua
+++ neovim/lazyvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,9 @@ return {
       bind_to_cwd = false,
       follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
+      filtered_items = {
+        visible = true,
+      },
     },
     window = {
       mappings = {
