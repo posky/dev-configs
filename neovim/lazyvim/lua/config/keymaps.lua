9a707d7a6968d7b3d5627c5fa542b07cdefbd14a
posky <pjm5810@gmail.com>
2023-09-26 16:02:12 +0900

    feat: lazyvim terminal keymaps

--- neovim/lazyvim/lua/config/keymaps.lua
+++ neovim/lazyvim/lua/config/keymaps.lua
@@ -1,4 +1,27 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
 vim.keymap.set("n", ";", ":", {})
+
+-- Terminal
+local lazyterm = function()
+  Util.float_term(nil, { cwd = Util.get_root() })
+end
+map("n", "<c-;>", lazyterm, { desc = "Terminal (root dir)" })
+map("t", "<C-;>", "<cmd>close<cr>", { desc = "Hide Terminal" })
