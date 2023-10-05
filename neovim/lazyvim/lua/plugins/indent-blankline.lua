43fb4eb2d0f7d28c63642e7d107c20c5422fbefc
posky <pjm5810@gmail.com>
2023-10-05 17:47:31 +0900

    fix: respond updated version

--- neovim/lazyvim/lua/plugins/indent-blankline.lua
+++ neovim/lazyvim/lua/plugins/indent-blankline.lua
@@ -1,25 +1,5 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
-  opts = {
-    -- char = "▏",
-    -- char = "│",
-    -- filetype_exclude = {
-    --   "help",
-    --   "alpha",
-    --   "dashboard",
-    --   "neo-tree",
-    --   "Trouble",
-    --   "lazy",
-    --   "mason",
-    --   "notify",
-    --   "toggleterm",
-    --   "lazyterm",
-    -- },
-    -- show_trailing_blankline_indent = false,
-    -- show_current_context = false,
-  },
-  setup = function()
-    require("ibl").setup()
-  end,
+  opts = {},
 }
