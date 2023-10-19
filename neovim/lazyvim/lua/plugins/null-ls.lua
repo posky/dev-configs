7696269c9144f3ae5d13ef90e2f62b646477dba6
posky <pjm5810@gmail.com>
2023-10-19 23:02:02 +0900

    feat: change null-ls to none-ls

--- neovim/lazyvim/lua/plugins/null-ls.lua
+++ neovim/lazyvim/lua/plugins/null-ls.lua
@@ -1,21 +1,20 @@
 return {
   "nvimtools/none-ls.nvim",
-  opts = function()
+  opts = function(_, opts)
     local nls = require("null-ls")
-    return {
-      root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-      sources = {
-        -- nls.builtins.formatting.fish_indent,
-        -- nls.builtins.diagnostics.fish,
-        nls.builtins.formatting.stylua,
-        nls.builtins.formatting.shfmt,
-        -- nls.builtins.diagnostics.flake8,
-        nls.builtins.formatting.prettierd,
-        nls.builtins.diagnostics.buf,
-        nls.builtins.formatting.buf,
-        nls.builtins.diagnostics.npm_groovy_lint,
-        -- nls.builtins.formatting.npm_groovy_lint,
-      },
-    }
+    opts.root_dir = opts.root_dir
+      or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
+    opts.sources = vim.list_extend(opts.sources or {}, {
+      -- nls.builtins.formatting.fish_indent,
+      -- nls.builtins.diagnostics.fish,
+      nls.builtins.formatting.stylua,
+      nls.builtins.formatting.shfmt,
+      -- nls.builtins.diagnostics.flake8,
+      nls.builtins.formatting.prettierd,
+      nls.builtins.diagnostics.buf,
+      nls.builtins.formatting.buf,
+      nls.builtins.diagnostics.npm_groovy_lint,
+      -- nls.builtins.formatting.npm_groovy_lint,
+    })
   end,
 }
