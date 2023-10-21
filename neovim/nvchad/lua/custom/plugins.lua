2809b3affe6c473a4372dec763bc8422f6a66ea9
posky <pjm5810@gmail.com>
2023-10-21 22:21:00 +0900

    fix(nvchad): change null-ls to none-ls

--- neovim/nvchad/lua/custom/plugins.lua
+++ neovim/nvchad/lua/custom/plugins.lua
@@ -9,9 +9,9 @@ local plugins = {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      "jose-elias-alvarez/null-ls.nvim",
+      "nvimtools/none-ls.nvim",
       config = function()
-        require "custom.configs.null-ls"
+        require "custom.configs.none-ls"
       end,
     },
 
