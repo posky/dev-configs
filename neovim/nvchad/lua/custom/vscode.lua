f79add5b0ceb21e42b6ea833f5edf1f23d7f9a7e
posky <pjm5810@gmail.com>
2023-11-29 13:11:02 +0900

    refactor(nvchad): vscode neovim

--- neovim/nvchad/lua/custom/vscode.lua
+++ neovim/nvchad/lua/custom/vscode.lua
@@ -1,52 +1,54 @@
 local opt = vim.opt
+local g = vim.g
+local keymap = vim.keymap
 local vscode = require "vscode-neovim"
 
 opt.ignorecase = true
 opt.smartcase = true
 
 -- keybindings
-vim.g.mapleader = " "
-vim.keymap.set("n", ";", ":", { noremap = true })
-vim.keymap.set({ "n", "v" }, "gd", function()
+g.mapleader = " "
+keymap.set("n", ";", ":", { noremap = true })
+keymap.set({ "n", "v" }, "gd", function()
   vscode.action("editor.action.revealDefinition", {})
 end)
-vim.keymap.set({ "n", "v" }, "gD", function()
+keymap.set({ "n", "v" }, "gD", function()
   vscode.action("editor.action.revealDefinitionAside", {})
 end)
-vim.keymap.set({ "n", "v" }, "gi", function()
+keymap.set({ "n", "v" }, "gi", function()
   vscode.action("editor.action.goToImplementation", {})
 end)
-vim.keymap.set({ "n", "v" }, "gr", function()
+keymap.set({ "n", "v" }, "gr", function()
   vscode.action("editor.action.goToReferences", {})
 end)
-vim.keymap.set({ "n", "v" }, "<leader>ra", function()
+keymap.set({ "n", "v" }, "<leader>ra", function()
   vscode.action("editor.action.rename", {})
 end)
-vim.keymap.set({ "n", "v" }, "<leader>ca", function()
+keymap.set({ "n", "v" }, "<leader>ca", function()
   vscode.action("editor.action.quickFix", {})
 end)
-vim.keymap.set({ "n", "v" }, "K", function()
+keymap.set({ "n", "v" }, "K", function()
   vscode.action("editor.action.showDefinitionPreviewHover", {})
 end)
-vim.keymap.set("n", "<leader>ss", function()
+keymap.set("n", "<leader>ss", function()
   vscode.action("workbench.action.gotoSymbol", {})
 end)
 
-vim.keymap.set("n", "]d", function()
+keymap.set("n", "]d", function()
   vscode.call "editor.action.marker.next"
 end)
-vim.keymap.set("n", "[d", function()
+keymap.set("n", "[d", function()
   vscode.call "editor.action.marker.prev"
 end)
-vim.keymap.set("n", "<leader>ff", function()
+keymap.set("n", "<leader>ff", function()
   vscode.call "workbench.action.quickOpen"
 end)
-vim.keymap.set("n", "<leader>fw", function()
+keymap.set("n", "<leader>fw", function()
   vscode.call "workbench.action.findInFiles"
 end)
-vim.keymap.set({ "n", "v" }, "<leader>/", function()
+keymap.set({ "n", "v" }, "<leader>/", function()
   vscode.call "editor.action.commentLine"
 end)
-vim.keymap.set("n", "<leader>x", function()
+keymap.set("n", "<leader>x", function()
   vscode.call "workbench.action.closeActiveEditor"
 end)
