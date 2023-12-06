87215482e3b0ee3a43b22d490a97264a5921a02a
posky <pjm5810@gmail.com>
2023-12-06 14:22:10 +0900

    feat(nvchad): vscode-neovim add plugins

--- neovim/nvchad/lua/custom/vscode.lua
+++ neovim/nvchad/lua/custom/vscode.lua
@@ -53,3 +53,32 @@ end)
 keymap.set("n", "<leader>x", function()
   vscode.call "workbench.action.closeActiveEditor"
 end)
+
+-- lazy
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup()
+    end,
+  },
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
