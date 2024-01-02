009622dd1b82cfa81b3caa365599edc621830b35
posky <pjm5810@gmail.com>
2024-01-02 17:55:16 +0900

    feat(wezterm): add key bindings

--- terminal/wezterm/.wezterm.lua
+++ terminal/wezterm/.wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 wezterm.on("update-right-status", function(window, pane)
 	window:set_right_status(window:active_workspace())
@@ -29,6 +30,53 @@ config.font = wezterm.font_with_fallback({
 	"Hack",
 	"DejaVu Sans Mono",
 })
+
+-- keybindings
+config.keys = {
+	-- Switch to the default active_workspace
+	{
+		key = "y",
+		mods = "CTRL|SHIFT",
+		action = act.SwitchToWorkspace({
+			name = "default",
+		}),
+	},
+	-- Create a new workspace with a random name and switch to it
+	{
+		key = "i",
+		mods = "CTRL|SHIFT",
+		action = act.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Enter name for new workspace" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be 'nil' if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:perform_action(
+						act.SwitchToWorkspace({
+							name = line,
+						}),
+						pane
+					)
+				end
+			end),
+		}),
+	},
+	{ key = "1", mods = "ALT", action = act.ShowLauncher },
+	-- Show the launcher in fuzzy selection mode and have it list all workspaces
+	-- and allow activating one.
+	{
+		key = "9",
+		mods = "ALT",
+		action = act.ShowLauncherArgs({
+			flags = "FUZZY|WORKSPACES",
+		}),
+	},
+}
 
 -- and finally, return the configuration to wezterm
 return config
