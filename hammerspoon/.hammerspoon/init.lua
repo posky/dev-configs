417797942b81194aa3782504facdcb2392cd5a9f
posky <pjm5810@gmail.com>
2024-01-26 13:02:25 +0900

    fix(hammerspoon): change default eng keyboard layout

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -18,19 +18,19 @@ remapper:remap(0x39, "rctrl")
 remapper:register()
 
 -- escape key mapping for vim
--- local input_eng = "com.apple.keylayout.ABC"
-local input_eng = "com.apple.keylayout.UnicodeHexInput"
+local input_eng = "com.apple.keylayout.ABC"
+-- local input_eng = "com.apple.keylayout.UnicodeHexInput"
 local esc_bind
 
 function convert_to_eng()
-	local cur_input = hs.keycodes.currentSourceID()
-	if not (cur_input == input_eng) then
-		-- hs.eventtap.keyStroke({}, "right")
-		hs.keycodes.currentSourceID(input_eng)
-	end
-	esc_bind:disable()
-	hs.eventtap.keyStroke({}, "escape")
-	esc_bind:enable()
+  local cur_input = hs.keycodes.currentSourceID()
+  if not (cur_input == input_eng) then
+    -- hs.eventtap.keyStroke({}, "right")
+    hs.keycodes.currentSourceID(input_eng)
+  end
+  esc_bind:disable()
+  hs.eventtap.keyStroke({}, "escape")
+  esc_bind:enable()
 end
 
 esc_bind = hs.hotkey.new({}, "escape", convert_to_eng):enable()
