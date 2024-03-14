83f39105c47909d769ccba73ac0f86bce6e35c6c
posky <pjm5810@gmail.com>
2024-03-14 16:19:49 +0900

    fix(neovim): change vscode keymap

--- neovim/lazyvim_2024/lua/config/keymaps.lua
+++ neovim/lazyvim_2024/lua/config/keymaps.lua
@@ -1,3 +1,30 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+if vim.g.vscode then
+  local keymap = vim.keymap
+  local vscode = require("vscode-neovim")
+
+  keymap.set({ "n", "v" }, "gD", function()
+    vscode.action("editor.action.revealDefinitionAside", {})
+  end)
+  keymap.set({ "n", "v" }, "gi", function()
+    vscode.action("editor.action.goToImplementation", {})
+  end)
+  keymap.set({ "n", "v" }, "gr", function()
+    vscode.action("editor.action.goToReferences", {})
+  end)
+  keymap.set({ "n", "v" }, "<leader>cr", function()
+    vscode.action("editor.action.rename", {})
+  end)
+  keymap.set({ "n", "v" }, "<leader>ca", function()
+    vscode.action("editor.action.quickFix", {})
+  end)
+
+  keymap.set("n", "]d", function()
+    vscode.action("editor.action.marker.nextInFiles", {})
+  end)
+  keymap.set("n", "[d", function()
+    vscode.action("editor.action.marker.prevInFiles", {})
+  end)
+end
