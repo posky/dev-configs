e2dc09e144a4a7053728572a4aa0ffc9b7859fe8
posky <pjm5810@gmail.com>
2024-05-29 12:53:53 +0900

    fix(lazyvim): remove cmp-emoji and venv-selector config

--- neovim/lazyvim_2024/lua/plugins/venv-selector-nvim.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "linux-cultist/venv-selector.nvim",
-  opts = function(_, opts)
-    if type(opts.name) == "table" then
-      vim.list_extend(opts.name, { ".hatch" })
-    end
-  end,
-}
