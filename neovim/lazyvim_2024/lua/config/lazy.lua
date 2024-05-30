ed4fa954046b87428399bd4b9d8ca2179001aabb
posky <pjm5810@gmail.com>
2024-05-30 17:33:10 +0900

    feat(lazyvim): add lang toml

--- neovim/lazyvim_2024/lua/config/lazy.lua
+++ neovim/lazyvim_2024/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.vscode" },
     -- import/override with your plugins
