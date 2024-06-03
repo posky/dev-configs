dddb8dceaba840be48d317668fd1c06b8dc51b90
posky <pjm5810@gmail.com>
2024-06-03 17:16:51 +0900

    fix(sketchybar): change date format

--- macos/sketchybar/plugins/calendar.sh
+++ macos/sketchybar/plugins/calendar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sketchybar --set $NAME icon="$(date '+%m %d %a')" label="$(date '+%H:%M:%S')"
+sketchybar --set $NAME icon="$(LANG=ko_KR.UTF-8 date '+%-m월 %-d일 %a')" label="$(date '+%H:%M:%S')"
