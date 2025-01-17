4b26ae4890a4f41f8b52d22a29674fc83be3d673
posky <pjm5810@gmail.com>
2025-01-17 14:38:55 +0900

    chore(zed): check code command exist

--- zed/diff.sh
+++ zed/diff.sh
@@ -4,5 +4,9 @@ SETTING_PATH="$HOME/.config/zed/settings.json"
 
 if [ -f "$SETTING_PATH" ]; then
   echo "Diff Zed setting"
-  code --diff "$BASEDIR/settings.json" "$SETTING_PATH"
+  if command -v code > /dev/null 2>&1; then
+    code --diff "$BASEDIR/settings.json" "$SETTING_PATH"
+  else
+    vim -d "$BASEDIR/settings.json" "$SETTING_PATH"
+  fi 
 fi
